b7d7395d91591d86b53af4fb318039d58dbeb3ac
Amit Gupta <iiita.amitgupta@gmail.com>
2020-10-07 11:36:35 +0530

    Configuring route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
+
+   namespace :api do
+    namespace :v1 do
+      resources :articles
+    end
+  end
+  # resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
